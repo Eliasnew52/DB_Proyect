40b0c2fddec1afd859c527668bbb71db45a69724
xSarscov <vargasernest7@gmail.com>
2024-11-21 09:15:15 -0600

    add list and create product feature

--- proyecto_db/proyecto_db/settings.py
+++ proyecto_db/proyecto_db/settings.py
@@ -40,6 +40,9 @@ INSTALLED_APPS = [
     
     #Apps
     'backend',
+    'registration',
+    'widget_tweaks',
+
 ]
 
 MIDDLEWARE = [
@@ -128,3 +131,7 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_URL = 'login'
+LOGIN_REDIRECT_URL = 'login'
+LOGOUT_REDIRECT_URL = 'login'
