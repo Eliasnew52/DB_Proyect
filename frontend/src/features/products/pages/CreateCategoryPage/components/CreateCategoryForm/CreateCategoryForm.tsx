911062b674ab3ea41cd65127e7329fc0e57b8707
xSarscov <vargasernest7@gmail.com>
2025-05-23 05:29:09 -0600

    feat: Implement CreateCategoryForm component for category creation

--- frontend/src/features/products/pages/CreateCategoryPage/components/CreateCategoryForm/CreateCategoryForm.tsx
+++ frontend/src/features/products/pages/CreateCategoryPage/components/CreateCategoryForm/CreateCategoryForm.tsx
@@ -1,14 +1,16 @@
-import {useCallback} from "react";
-import {Controller, useForm} from "react-hook-form";
-import {ContentContainer} from "../../../../../common/components/ui/ContentContainer.tsx";
+import {useCallback, useState} from "react";
+import {Controller, useFieldArray, useForm} from "react-hook-form";
 import {Box, Button, FormHelperText, Grid, InputLabel, TextField, Typography} from "@mui/material";
+import {ContentContainer} from "../../../../../common/components/ui/ContentContainer.tsx";
 import {Category} from "../../../types/categories.types.ts";
 import FileUploadIcon from '@mui/icons-material/FileUpload';
+import SettingsIcon from '@mui/icons-material/Settings';
 
 export const CreateCategoryForm = () => {
 
+    const [isModalOpen, setIsModalOpen] = useState(false);
+
     const {
-        register,
         handleSubmit,
         formState: { errors },
         control,
@@ -17,8 +19,14 @@ export const CreateCategoryForm = () => {
             name: '',
             description: '',
             image: undefined,
+            characteristics: []
         }
     });
+
+    const { fields, append, remove } = useFieldArray({
+        control,
+        name: "characteristics",
+    })
 
     const onSubmit = useCallback((data: Partial<Category>) => {
 
@@ -104,6 +112,31 @@ export const CreateCategoryForm = () => {
                         </Box>
                     )}
                 />
+
+                 <Grid>
+                     <Button variant={"text"} startIcon={<SettingsIcon />} onClick={() => append({ key: '', title: '', type: 'string', options: [] })}>
+                        Configura las características de los productos
+                     </Button>
+                 </Grid>
+
+                {
+                    fields.map((field, index) => (
+                        <Grid
+                            key={field.id}
+                            container
+                            flexDirection={'column'}
+                        >
+                            <Controller
+                                name={`characteristics.${index}.key`}
+                                control={control}
+                                render={({ field }) => (
+                                    <TextField {...field} label="Nombre del campo" fullWidth />
+                                )}
+                            />
+                        </Grid>
+                    ))
+                }
+
             </Grid>
         </ContentContainer>
     )
